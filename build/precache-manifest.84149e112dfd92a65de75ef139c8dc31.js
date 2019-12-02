6f1c3981bf60a4ad818d56ee59ca946dfdfd4a1b
= <jonboone1@gmail.com>
2019-12-02 15:52:43 -0800

    attempting to use the build functionality

---
+++ build/precache-manifest.84149e112dfd92a65de75ef139c8dc31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a694d4c1fbce8b38094ea7dd232ec893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92f8f276a14db5a2e5c4",
+    "url": "/static/css/2.16aca965.chunk.css"
+  },
+  {
+    "revision": "92f8f276a14db5a2e5c4",
+    "url": "/static/js/2.a80f054e.chunk.js"
+  },
+  {
+    "revision": "88251c9fa9bb7768fd87",
+    "url": "/static/js/main.312c72a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
